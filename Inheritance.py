a4c3d42ae7d62ba76aea9066c381ac68fe144569
Rohit <rkumar8092378845@gmail.com>
2021-04-24 12:20:42 +0530

    -> Added class for demonstrating multi-level inheritance in Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -1,4 +1,5 @@
 # Inheritance allows us to define class that inherits all the methods and properties from another class
+# ====================================================================
 class Person:
     def __init__(self, first_name, last_name, age):
         self.firstname = first_name
@@ -10,9 +11,13 @@ class Person:
         print("Age : " + str(self.age))
 
 
+# -------------------------------------------------------------------------------------
+
 class Test(Person):
     pass
 
+
+# ----------------------------Single inheritance --------------------------------------
 
 # Using child class  __init__ method
 class Teacher(Person):
@@ -25,6 +30,7 @@ class Teacher(Person):
         print("Salary : " + str(self.salary))
 
 
+# ----------------------------Single inheritance --------------------------------------
 class Student(Person):
     def __init__(self, first_name, last_name, age, roll_number, *subjects):
         super().__init__(first_name, last_name, age)  # using super() method , super() refer to the parent class
@@ -38,8 +44,11 @@ class Student(Person):
         print("Main subject : " + str(self.subjects[2]))
 
 
-# Multiple inheritance
+# -------------------------------------------------------------------------------------
 
+# -------------------------- Multiple inheritance --------------------------------------
+
+# Multiple inheritance
 class Base1:
     def __init__(self):
         self.str1 = "Base1"
@@ -60,6 +69,38 @@ class Derived(Base1, Base2):
         Base2.__init__(self)
 
 
+# -------------------------------------------------------------------------------------
+
+# ----------------------------Multi level inheritance ------------------------------
+
+class Parent:
+    def __init__(self, name):
+        self.name = name
+
+    def getName(self):
+        return self.name
+
+
+class Child(Parent):
+    def __init__(self, name, age):
+        Parent.__init__(self, name)
+        self.age = age
+
+    def getAge(self):
+        return self.age
+
+
+class GrandChild(Child):
+    def __init__(self, name, age, address):
+        Child.__init__(self, name, age)
+        self.address = address
+
+    def getAddress(self):
+        return self.address
+
+
+# -------------------------------------------------------------------------------------
+
 # Using class
 t = Test("Rohit", "Kumar", 34)
 t.printPersonDetails()
@@ -73,4 +114,12 @@ student = Student("Sagar", "Singh", 13, 45, "english", "hindi", "maths", "scienc
 student.printStudentDetails()
 print()
 
+print("Demonstrating Multiple inheritance")
 derived = Derived()  # making the derived class object
+print()
+
+print("Demonstrating Multilevel inheritance")
+grandChild = GrandChild("Rohit", 24, "Vidyanagar")
+print("Name = " + grandChild.getName())
+print("Age = " + str(grandChild.getAge()))
+print("Address = " + grandChild.getAddress())
