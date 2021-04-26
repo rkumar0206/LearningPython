a476d5afa6635f2d968a06c1b981ccd1a37a61d6
Rohit <rkumar8092378845@gmail.com>
2021-04-26 23:54:00 +0530

    -> Working with FileDirectory.py in python

---
+++ FileDirectory.py
@@ -0,0 +1,25 @@
+import os
+
+# get current working directory
+print("Current working directory : " + os.getcwd())
+
+# changing directory
+# os.chdir("R:\\NewPythonFolder")
+
+# listing directory and files
+print(os.listdir())
+print(os.listdir("R:\\"))  # list of files and directories by specifying path
+
+# making new directory
+os.mkdir("test")
+print("made a new file test")
+
+# renaming a file
+os.rename('test', 'test_renamed')
+print("Renamed test to test_renamed")
+
+# we can remove a directory using rmdir('dir_name') method and for removing a file we can
+# use remove('file_name') method
+
+os.rmdir('test_renamed')
+print("'test_renamed file has been removed.")
